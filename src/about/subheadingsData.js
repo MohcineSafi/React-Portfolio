7254e666ea1ef48924b528f879dd2a6040160c1a
MohcineSafi <safi.mouhcine02@gmail.com>
2024-02-12 00:37:25 +0100

    Add My Portfolio React Code

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,53 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            I'm a passionate software engineer based in Kenitra ,Morocco. I love
+            to explore new technologies and create meaningful applications.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "2020 Baccalaureate Degree",
+      content: (
+        <>
+          <p>Baccalaureate degree in physical sciences</p>
+        </>
+      ),
+    },
+    {
+      title: "2021 RDRAAK Degree",
+      content: (
+        <>
+          <p>
+            Game development using Unity:In this specialization I will be able
+            to learn the C# programming language from scratch until I reach a
+            level where I can write lines and use
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "2022 Front End Developer",
+      content: (
+        <>
+          <p>
+            I want to create websites and web applications. As Front-End I want
+            to refer to how a web page looks, while as back-end I want to refer
+            to how it works.
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
