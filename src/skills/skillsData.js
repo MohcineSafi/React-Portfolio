00d8d390713d2b234e260a2ab880b09896c822de
MohcineSafi <safi.mouhcine02@gmail.com>
2024-02-13 00:06:23 +0100

    Add My Portfolio React Code

---
+++ src/skills/skillsData.js
@@ -0,0 +1,68 @@
+const skills = {
+  1: [
+    {
+      title: "HTML",
+      level: 6,
+    },
+    {
+      title: "CSS",
+      level: 6,
+    },
+    {
+      title: "Bootstrap",
+      level: 5,
+    },
+    {
+      title: "JavaScript",
+      level: 5,
+    },
+    {
+      title: "TypeScript",
+      level: 4,
+    },
+    {
+      title: "Tailwind CSS",
+      level: 5,
+    },
+    {
+      title: "React JS",
+      level: 4,
+    },
+    {
+      title: "Figma",
+      level: 5,
+    },
+  ],
+  2: [
+    {
+      title: "PostgreSQL",
+      level: 3,
+    },
+    {
+      title: "Node.js",
+      level: 4,
+    },
+    {
+      title: "MongoDB",
+      level: 4,
+    },
+    {
+      title: "Git",
+      level: 5,
+    },
+    {
+      title: "PHP",
+      level: 5,
+    },
+    {
+      title: "SQL",
+      level: 6,
+    },
+    {
+      title: "Laravel",
+      level: 4,
+    },
+  ],
+};
+
+export default skills;
